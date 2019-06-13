446c46b50ed330326699fa7584e6370a109d63aa
Roltaire Solis <rsolis.vdw@gmail.com>
2019-06-13 16:27:44 -0700

    Fixed jquery version

--- wp-content/themes/shapely-child/functions.php
+++ wp-content/themes/shapely-child/functions.php
@@ -6,10 +6,10 @@ if(! function_exists('swopelaw_scripts')) :
 
         $stylesheetURI = get_stylesheet_directory_uri();
 
-        wp_deregister_script( 'jquery' );
-        // Change the URL if you want to load a local copy of jQuery from your own server.
-        wp_register_script( 'jquery', "https://code.jquery.com/jquery-3.4.1.min.js", array(), '3.4.1', false );
-        wp_enqueue_script('jquery');
+        // wp_deregister_script( 'jquery' );
+        // // Change the URL if you want to load a local copy of jQuery from your own server.
+        // wp_register_script( 'jquery', "https://code.jquery.com/jquery-3.4.1.min.js", array(), '3.4.1', false );
+        // wp_enqueue_script('jquery');
 
         wp_enqueue_style('slick-style', $stylesheetURI."/assets/js/slick-1.8.1/slick/slick.css");
         wp_enqueue_style('slick-theme', $stylesheetURI."/assets/js/slick-1.8.1/slick/slick-theme.css");
